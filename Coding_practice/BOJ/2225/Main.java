60d5d1d1e789b950bbfeefb2810662b8b349e387
dev1 <yozdaz@gmail.com>
2025-01-22 17:44:58 +0900

    .

--- Coding_practice/BOJ/2225/Main.java
+++ Coding_practice/BOJ/2225/Main.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class P2225 {
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         final int div = 1000000000;
