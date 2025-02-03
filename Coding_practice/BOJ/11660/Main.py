01e32e3967eb7bd7b087698fe224368d6095a2f6
cyanjz <yozdaz@gmail.com>
2025-02-03 21:22:18 +0900

    .

--- Coding_practice/BOJ/11660/Main.py
+++ Coding_practice/BOJ/11660/Main.py
@@ -1,9 +1,17 @@
 N, M = map(int, input().split())
-table = [[map(int, input().split)] for _ in range(M)]
+table = [list(map(int, input().split())) for _ in range(N)]
 
 def solve():
+    sum_matrix = [[0] * (N+1)] + [[0] + [table[r][0]] + [0]*(N-1) for r in range(N)]
+    for r in range(1, N+1):
+        for c in range(1, N+1):
+            sum_matrix[r][c] = sum_matrix[r][c-1] + table[r-1][c-1]
+    for c in range(1, N+1):
+        for r in range(1, N+1):
+            sum_matrix[r][c] = sum_matrix[r-1][c] + sum_matrix[r][c]
     for _ in range(M):
-        
+        r1, c1, r2, c2 = map(int, input().split())
+        print(sum_matrix[r2][c2]-sum_matrix[r2][c1-1]-sum_matrix[r1-1][c2]+sum_matrix[r1-1][c1-1])
     
 
 if __name__ == '__main__':
