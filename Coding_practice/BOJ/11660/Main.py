c4ba30945d8066eec419f97fb40f1a81f0d0939e
dev1 <yozdaz@gmail.com>
2025-02-03 17:56:42 +0900

    .

---
+++ Coding_practice/BOJ/11660/Main.py
@@ -0,0 +1,10 @@
+N, M = map(int, input().split())
+table = [[map(int, input().split)] for _ in range(M)]
+
+def solve():
+    for _ in range(M):
+        
+    
+
+if __name__ == '__main__':
+    solve()
